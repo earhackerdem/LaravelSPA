6b1f82044d07bb51325745096f7b99fc7aacae4b
Earvin Saúl Pérez Ramos <earhackerdem@gmail.com>
2021-02-01 17:43:45 -0600

    Jetstream:configuración inicial

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,17 @@
 <?php
 
+use App\Http\Controllers\NoteController;
+use App\Http\Controllers\PageController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+Route::view('/', 'index');
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->name('dashboard');
+Route::get('dashboard',[PageController::class,'dashboard'])
+    ->middleware('auth:sanctum')
+    ->name('dashboard');
+
+Route::resource('notes',NoteController::class)
+    ->middleware('auth:sanctum');
